cbab2cc4ec57fe88e91f9bd31b01ead76bcfb26a
Fabrice Flore-Thébault <ffloreth@redhat.com>
2024-10-03 14:58:26 +0200

    document the script
    Signed-off-by: Fabrice Flore-Thébault <ffloreth@redhat.com>

--- build/scripts/single-source-security-fixes.sh
+++ build/scripts/single-source-security-fixes.sh
@@ -10,12 +10,21 @@
 # Fail and stop on first error
 set -e
 destination=modules/release-notes/snip-common-vulnerabilities-and-exposures.adoc
+# Cleanup the destination files
 rm "$destination"
-# Output
+# Send output to the destination file
 exec &>> "$destination"
 for cve in $(cat cve-list.txt)
 do
+  # Start the list.
   echo "link:https://access.redhat.com/security/cve/$cve[$cve]::"
+  # Call the API to return a list of details.
+  # Red Hat is last if there is one.
+  # Red Hat details is single line.
+  # MITRE details are multiline.
+  # We keep Red Hat details if present.
+  # We keep only the first two lines on MITRE details.
   curl -s "https://access.redhat.com/hydra/rest/securitydata/cve/$cve.json" | jq -r '.details[-1]' | head -n 2
+  # Add a separation
   echo ""
 done
