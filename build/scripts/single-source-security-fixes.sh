59f96ef1494816e64daa38e05414bc8ec42f16e3
Fabrice Flore-Thébault <ffloreth@redhat.com>
2024-10-03 13:45:45 +0200

    RHIDP-4290 - Update CVE list for release 1.3
    Signed-off-by: Fabrice Flore-Thébault <ffloreth@redhat.com>

---
+++ build/scripts/single-source-security-fixes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2024 Red Hat, Inc.
+# This program, and the accompanying materials are made
+# available under the terms of the Apache Public License 2.0,
+# available at http://www.apache.org/licenses/
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Fail and stop on first error
+set -e
+destination=modules/release-notes/snip-common-vulnerabilities-and-exposures.adoc
+rm "$destination"
+# Output
+exec &>> "$destination"
+for cve in $(cat cve-list.txt)
+do
+  echo "link:https://access.redhat.com/security/cve/$cve[$cve]:: "
+  curl -s "https://access.redhat.com/hydra/rest/securitydata/cve/$cve.json" | jq -r '.details[0]'
+  echo ""
+done
