ddda713c653d42d7cce32068dfffe06d9a6cc9b9
Fabrice Flore-Thébault <ffloreth@redhat.com>
2024-10-07 16:19:54 +0200

    docs: update CVE list
    Signed-off-by: Fabrice Flore-Thébault <ffloreth@redhat.com>

--- build/scripts/single-source-security-fixes.sh
+++ build/scripts/single-source-security-fixes.sh
@@ -7,15 +7,28 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+# Single-source the release notes Fixed security issues section from Red Hat Security Data API.
+# See: https://docs.redhat.com/en/documentation/red_hat_security_data_api/1.0/html/red_hat_security_data_api/cve
+
 # Fail and stop on first error
 set -e
-destination=modules/release-notes/snip-common-vulnerabilities-and-exposures.adoc
-# Cleanup the destination files
-rm "$destination"
-# Send output to the destination file
-exec &>> "$destination"
-for cve in $(cat cve-list.txt)
-do
+
+product_version="$(grep ':product-bundle-version:' artifacts/attributes.adoc | cut -d' ' -f2 )"
+
+single_source_from_security_data () {
+  # Assert that the list file exists.
+  if [ ! -f ${list} ]
+  then
+    echo "ERROR: The ${list} file is missing"
+    exit 1
+  fi
+  # Cleanup the destination files.
+  rm -f "$destination"
+  # Send output to the destination file.
+  exec &>> "$destination"
+  echo "= ${title}"
+  for cve in $(cat ${list} | sort | uniq)
+  do
   # Start the list.
   echo "link:https://access.redhat.com/security/cve/$cve[$cve]::"
   # Call the API to return a list of details.
@@ -27,4 +40,15 @@ do
   curl -s "https://access.redhat.com/hydra/rest/securitydata/cve/$cve.json" | jq -r '.details[-1]' | head -n 2
   # Add a separation
   echo ""
-done
+  done
+}
+
+title="{product} dependency updates"
+destination="modules/release-notes/snip-common-vulnerabilities-and-exposures-product-${product_version}.adoc"
+list="build/cve-lists/cve-list-product-${product_version}.txt"
+single_source_from_security_data
+
+title="RHEL 9 platform RPM updates"
+list="build/cve-lists/cve-list-rpm-${product_version}.txt"
+destination="modules/release-notes/snip-common-vulnerabilities-and-exposures-rpm-${product_version}.adoc"
+single_source_from_security_data
