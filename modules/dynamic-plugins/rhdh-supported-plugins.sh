9edaebf7ab45d5a60c344eab433158e5e4d55d72
Nick Boldt <nboldt@redhat.com>
2024-12-03 22:39:50 -0500

    re-add missing community plugins
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- modules/dynamic-plugins/rhdh-supported-plugins.sh
+++ modules/dynamic-plugins/rhdh-supported-plugins.sh
@@ -297,10 +297,12 @@ done
 num_plugins+=(${#adoc1[@]})
 
 # RHIDP-5103 - currently no tech-preview plugins, only community. So disable this for now
-# rm -f "${0/.sh/.adoc2}"
-# sorted=(); while IFS= read -rd '' key; do sorted+=( "$key" ); done < <(printf '%s\0' "${!adoc2[@]}" | sort -z)
-# for key in "${sorted[@]}"; do echo -e "${adoc2[$key]}" >> "${0/.sh/.ref-rh-tech-preview-plugins}"; echo -e "${csv[$key]}" >>  "${0/.sh/.csv}"; done
-# num_plugins+=(${#adoc2[@]})
+rm -f "${0/.sh/.adoc2}"
+sorted=(); while IFS= read -rd '' key; do sorted+=( "$key" ); done < <(printf '%s\0' "${!adoc2[@]}" | sort -z)
+for key in "${sorted[@]}"; do 
+    # echo -e "${adoc2[$key]}" >> "${0/.sh/.ref-rh-tech-preview-plugins}"; 
+    echo -e "${csv[$key]}" >>  "${0/.sh/.csv}"; done
+num_plugins+=(${#adoc2[@]})
 
 rm -f "${0/.sh/.adoc3}"
 sorted=(); while IFS= read -rd '' key; do sorted+=( "$key" ); done < <(printf '%s\0' "${!adoc3[@]}" | sort -z)
@@ -308,16 +310,18 @@ for key in "${sorted[@]}"; do echo -e "${adoc3[$key]}" >> "${0/.sh/.ref-communit
 num_plugins+=(${#adoc3[@]})
 
 # merge the content from the three .adocX files into the .template.adoc file, replacing the TABLE_CONTENT markers
-count=0
+count=1
+index=0
 for d in ref-rh-supported-plugins ref-community-plugins; do # RHIDP-5103 - remove ref-rh-tech-preview-plugins for now as everything has moved to community
-    this_num_plugins=${num_plugins[$count]}
-    (( count = count + 1 ))
+    (( index = count - 1 ))
+    this_num_plugins=${num_plugins[$index]}
     echo "[$count] Processing $d ..."
     adocfile="${0/.sh/.${d}}"
     sed -e "/%%TABLE_CONTENT_${count}%%/{r $adocfile" -e 'd}' \
         -e "s/\%\%COUNT_${count}\%\%/$this_num_plugins/" \
         "${0/rhdh-supported-plugins.sh/${d}.template.adoc}" > "${0/rhdh-supported-plugins.sh/${d}.adoc}"
     rm -f "$adocfile"
+    (( count = count + 2 ))
 done
 
 # inject ENABLED_PLUGINS into con-preinstalled-dynamic-plugins.template.adoc
