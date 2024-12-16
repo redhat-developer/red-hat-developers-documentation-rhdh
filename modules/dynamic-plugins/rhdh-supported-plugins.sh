2a047897f69ed8f03cfedad95ab62ace23d8defc
Nick Boldt <nboldt@redhat.com>
2024-12-16 14:31:50 -0500

    chore(dynamic plugins): fix scope references for rhdh-plugins: should be @red-hat-developer-hub/ not @red/hat-developer-hub-
    Signed-off-by: Nick Boldt <nboldt@redhat.com>
    
    regen csv and adoc
    
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- modules/dynamic-plugins/rhdh-supported-plugins.sh
+++ modules/dynamic-plugins/rhdh-supported-plugins.sh
@@ -121,6 +121,7 @@ for j in $jsons; do
     if [[ $Plugin != "@"* ]]; then # don't update janus-idp/backstage-plugins plugin names
         Plugin="$(echo "${Plugin}" | sed -r -e 's/([^-]+)-(.+)/\@\1\/\2/' \
             -e 's|janus/idp-|janus-idp/|' \
+            -e 's|red/hat-developer-hub-|red-hat-developer-hub/|' \
             -e 's|backstage/community-|backstage-community/|' \
             -e 's|parfuemerie/douglas-|parfuemerie-douglas/|')"
     fi
@@ -179,8 +180,9 @@ for j in $jsons; do
         # curl -sSLko- https://registry.npmjs.org/@janus-idp%2fcli | jq -r '.versions[]|(.version+", "+.gitHead)' | sort -uV
         # for timestamp when tag is created
         # curl -sSLko- https://registry.npmjs.org/@janus-idp%2fcli | jq -r '.time' | grep -v -E "created|modified|{|}" | sort -uV
+        # echo "Searching for ${Plugin/\//%2f} at npmjs.org..."
         allVersionsPublished="$(curl -sSLko- "https://registry.npmjs.org/${Plugin/\//%2f}" | jq -r '.versions[].version')"
-        # echo $allVersionsPublished
+        # echo "Found $allVersionsPublished"
         # clean out any pre-release versions
         latestXYRelease="$(echo "$allVersionsPublished" | grep -v -E -- "next|alpha|-" | grep -E "^${Version%.*}" | sort -uV | tail -1)"
         # echo "[DEBUG] Latest x.y version at https://registry.npmjs.org/${Plugin/\//%2f} : $latestXYRelease"
@@ -295,16 +297,24 @@ num_plugins+=(${#adoc1[@]})
 
 rm -f "${0/.sh/.adoc2}"
 sorted=(); while IFS= read -rd '' key; do sorted+=( "$key" ); done < <(printf '%s\0' "${!adoc2[@]}" | sort -z)
-for key in "${sorted[@]}"; do 
-    echo -e "${adoc2[$key]}" >> "${0/.sh/.ref-rh-tech-preview-plugins}"; 
-    echo -e "${csv[$key]}" >>  "${0/.sh/.csv}"; done
+# shellcheck disable=SC2128
+if [[ $sorted ]] ;then 
+    for key in "${sorted[@]}"; do 
+        echo -e "${adoc2[$key]}" >> "${0/.sh/.ref-rh-tech-preview-plugins}"; 
+        echo -e "${csv[$key]}" >>  "${0/.sh/.csv}"
+    done
+fi
 num_plugins+=(${#adoc2[@]})
 
 rm -f "${0/.sh/.adoc3}"
 sorted=(); while IFS= read -rd '' key; do sorted+=( "$key" ); done < <(printf '%s\0' "${!adoc3[@]}" | sort -z)
-for key in "${sorted[@]}"; do 
-    echo -e "${adoc3[$key]}" >> "${0/.sh/.ref-community-plugins}"; 
-    echo -e "${csv[$key]}" >>  "${0/.sh/.csv}"; done
+# shellcheck disable=SC2128
+if [[ $sorted ]] ;then 
+    for key in "${sorted[@]}"; do 
+        echo -e "${adoc3[$key]}" >> "${0/.sh/.ref-community-plugins}"; 
+        echo -e "${csv[$key]}" >>  "${0/.sh/.csv}"
+    done
+fi
 num_plugins+=(${#adoc3[@]})
 
 # merge the content from the three .adocX files into the .template.adoc file, replacing the TABLE_CONTENT markers
